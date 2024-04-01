45d5c1e7b03e892226d93bf0f8af10bdc767ab4a
André <andrefs894@gmail.com>
2024-04-01 18:39:15 +0100

    updating project

---
+++ script.js
@@ -0,0 +1,29 @@
+let plants = [
+    {
+        name:"plant1",
+        price:10,
+    },
+    {
+        name:"plant2",
+        price:10,
+    },
+    {
+        name:"plant3",
+        price:10,
+    },
+    {
+        name:"plant4",
+        price:10,
+    },
+    {
+        name:"plant5",
+        price:10,
+    },
+]
+
+function generatePlants(array) {
+    let items = "";
+    for(let i = 0; i < array.length; i++) {
+        items += `<li></li>`
+    }
+}
